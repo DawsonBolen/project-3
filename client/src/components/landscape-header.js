e7fa9aa6ea6d03cd67bcc87b4c08c299329d84db
DawsonBolen <dawson120@live.com>
2023-08-27 01:48:15 -0700

    added some styling and fixed a few bugs. The images should be working now although i may still need to change a few more

--- client/src/components/landscape-header.js
+++ client/src/components/landscape-header.js
@@ -6,37 +6,37 @@ const LandscapeHeader = () => {
         <header>
             <nav className='main-nav'>
 
-                <img src='images/square-share-logo.png' height='75px'></img>
+                <img src={process.env.PUBLIC_URL + '/images/square-share-logo.png'} height='75px'></img>
 
 
                 <div className='search'>
                     <form className='search-bar'>
                         <input type='text' placeholder='Search' className='search-string'></input>
                         <button className='search-button'>
-                            <img src='images/newsearchicon.png' width='20px'></img>
+                            <img src={process.env.PUBLIC_URL + '/images/newsearchicon.png'} width='20px'></img>
                         </button>
                     </form>
                 </div>
                 <div className='user'>
-                    <img src='images/user-icon.png' width='45px'></img>
+                    <img src={process.env.PUBLIC_URL + '/images/user-icon.png'} width='45px'></img>
                 </div>
 
                 <div className='main-links'>
-                    <Link className='nav-link-style' to='/CreateSquare'>
+                    <Link className='nav-link-style' to='/Post'>
                         <div className='post'>
-                            <img src='images/post.png' height='25px'></img>
+                            <img src={process.env.PUBLIC_URL + '/images/post.png'} height='25px'></img>
                         </div>
                     </Link>
                     <div className='save'>
-                        <img src='images/save.png' height='25px'></img>
+                        <img src={process.env.PUBLIC_URL + '/images/save.png'} height='25px'></img>
                     </div>
 
                     <div className='explore'>
-                        <img src='images/explore.png' height='25px'></img>
+                        <img src={process.env.PUBLIC_URL + '/images/explore.png'} height='25px'></img>
                     </div>
                     <Link className='nav-link-style' to='/Home'>
                         <div className='home'>
-                            <img src='images/home-icon.png' height='25px'></img>
+                            <img src={process.env.PUBLIC_URL + '/images/home-icon.png'} height='25px'></img>
                         </div>
                     </Link>
                 </div>
